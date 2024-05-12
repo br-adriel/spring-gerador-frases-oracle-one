bdb93a554e02bcfcba128137d018b0649f9a6608
Adriel <adriel.fsantos@outlook.com>
2024-05-11 22:59:11 -0300

    Create project

---
+++ src/main/java/br/com/alura/gerafrase/GeraFraseApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.gerafrase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeraFraseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeraFraseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/gerafrase/GeraFraseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.gerafrase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeraFraseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
